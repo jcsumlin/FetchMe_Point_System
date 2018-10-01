0b8cbb87dc330822594bdc3641ecd38bbe2b9c4f
J_C___ <jcsumlin@gmail.com>
2018-10-01 13:52:11 -0500

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jcsum\PycharmProjects\FetchMe_Point_System\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
